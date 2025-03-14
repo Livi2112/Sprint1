9e93b485ef78fed315f14a5289651d025f50eeec
Livi2112 <livipedro2112@gmail.com>
2025-03-14 16:28:50 -0300

    Calculadora V2 (versão final - sistema de estados)

--- script.js
+++ script.js
@@ -1,27 +1,79 @@
-function displayConta(input) {
-    if (document.getElementById("conta").textContent == 0)
-        document.getElementById("conta").textContent = "";
-    conta.textContent += input;
-    resultado.textContent = eval(conta.textContent);
+//o state pode variar entre 3 estados: equ, op, num
+var state = "equ";
+var conta = "0";
+let contaDisplay = document.getElementById("contaDisplay").textContent;
+let result = document.getElementById("resultado");
+state = "num";
+
+//essa função checa os possíveis estados e adiciona os input de tipo número
+function num(input) {
+    console.log(state);
+
+    if (state == "equ") {
+        conta = input;
+        document.getElementById("contaDisplay").textContent = input;
+        document.getElementById("resultado").textContent = eval(conta);
+    }
+    else if (state == "op") {
+        conta += input;
+        document.getElementById("contaDisplay").textContent += input;
+        document.getElementById("resultado").textContent = eval(conta);
+    }
+    else {
+        conta += input;
+        document.getElementById("contaDisplay").textContent += input;
+        document.getElementById("resultado").textContent = eval(conta);
+    }
+    state = "num";
 }
 
+//essa função checa os possíveis estados e adiciona os inputs de tipo operadore
+function op(input) {
+    if (state == "equ") {
+        conta += input;
+        document.getElementById("contaDisplay").textContent += input;
+    }
+    else if (state == "op") {
+        return;
+    }
+    else {
+        conta += input;
+        document.getElementById("contaDisplay").textContent += input;
+    }
+    state = "op";
+}
+
+//função da tecla igual
+function equ() {
+    conta = eval(conta);
+    document.getElementById("contaDisplay").textContent = conta;
+    document.getElementById("resultado").textContent = conta;
+    state = "equ";
+}
+
+//essa função limpa a tela
 function limparDisplay() {
-    conta.textContent = 0;
+    conta = 0;
+    document.getElementById("contaDisplay").textContent = 0;
     document.getElementById("resultado").textContent = 0;
+    state = "equ"
 }
 
-function mostrarResultado() {
-    resultado.textContent = eval(conta.textContent);
-    conta.textContent = eval(conta.textContent);
-}
-
-function apagar() {
-    let conta = document.getElementById("conta").textContent;
-    document.getElementById("conta").textContent = conta.substring(0, conta.length -1);
-    if (document.getElementById("conta").textContent == "")
-        document.getElementById("conta").textContent = 0;
-}
-
+//essa função ativa a função igual e troca o sinal
 function mudarSinal() {
-    document.getElementById("conta").textContent = (-1) * document.getElementById("conta").textContent;
+    equ();
+    conta = (-1) * conta;
+    document.getElementById("contaDisplay").textContent = conta;
+    document.getElementById("resultado").textContent = conta;
+}
+
+//essa função apaga o último caractere da string
+function apagar() {
+    if (conta.length != 1) {
+    conta = conta.slice(0, -1);
+    }
+    else {conta = 0}
+    document.getElementById("contaDisplay").textContent = conta;
+    document.getElementById("resultado").textContent = conta;
+    state = "num"
 }
